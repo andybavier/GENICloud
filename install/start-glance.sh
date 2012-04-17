1c685af1d974ff01f75a2bd006b255b1c7fd4887
Andy Bavier <acb@cs.princeton.edu>
2012-04-17 17:21:27 -0400

    Show status of services

--- install/start-glance.sh
+++ install/start-glance.sh
@@ -3,4 +3,6 @@
 for svc in api registry
 do
     service openstack-glance-$svc start
+    sleep 1
+    service openstack-glance-$svc status
 done
--- install/start-nova.sh
+++ install/start-nova.sh
@@ -3,6 +3,8 @@
 for svc in api objectstore compute network volume scheduler
 do 
     service openstack-nova-$svc start
+    sleep 1
+    service openstack-nova-$svc status
 done
 
 
